4b7cdfe44201a52062579f38c794c0cf16dc5367
Morten Klim Sorensen <mail@kl1m.dk>
2013-03-11 10:15:22 +0100

    Changes to repo made

---
+++ Repository/Resources/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    public class UserRepository
+    {
+        private BPDbContext db;
+
+        public UserRepository()
+        {
+            db = new BPDbContext();
+        }
+
+        public IQueryable<User> GetAllUsers()
+        {
+            return db.Users;
+        }
+
+        public User GetUserById(int id)
+        {
+            return db.Users.FirstOrDefault(x => x.ID == id);
+        }
+
+        public void Insert(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        public void Update(User user)
+        {
+            User rUser = GetUserById(user.ID);
+            if (rUser == null)
+                return;
+
+            rUser.Email = user.Email;
+            rUser.IsActive = user.IsActive;
+            rUser.UserName = user.UserName;
+            db.SaveChanges();
+        }
+
+        public void Delete(User user)
+        {
+            if (GetUserById(user.ID) == null)
+                return;
+            db.Users.Remove(user);
+            db.SaveChanges();
+        }
+    }
+}
--- WebClient/Global.asax.cs
+++ WebClient/Global.asax.cs
@@ -16,7 +16,7 @@ namespace WebClient
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
