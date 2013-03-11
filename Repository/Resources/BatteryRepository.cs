0480c72df8425cc423bf097a800aa7d5bf797d8e
Chris <christind@hotmail.com>
2013-03-11 13:08:06 +0100

    Added BatteryRepository

---
+++ Repository/Resources/BatteryRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    class BatteryRepository
+    {
+        private BPDbContext db;
+
+        public BatteryRepository()
+        {
+            db = new BPDbContext();
+        }
+
+        public IQueryable<Battery> GetAllBatteries()
+        {
+            return db.Batteries;
+        }
+
+        public Battery GetBatteryById(int id)
+        {
+            return db.Batteries.FirstOrDefault(x => x.ID == id);
+        }
+
+        public void Insert(Battery battery)
+        {
+            db.Batteries.Add(battery);
+            db.SaveChanges();
+        }
+
+        public void Update(Battery battery)
+        {
+            Battery rBattery = GetBatteryById(battery.ID);
+            if (rBattery == null)
+                return;
+
+            rBattery.OutOfCommisionDate = battery.OutOfCommisionDate;
+            rBattery.IsActive = battery.IsActive;
+            db.SaveChanges();
+        }
+
+        public void Delete(Battery battery)
+        {
+            if (GetBatteryById(battery.ID) == null)
+                return;
+            db.Batteries.Remove(battery);
+            db.SaveChanges();
+        }
+    }
+}
