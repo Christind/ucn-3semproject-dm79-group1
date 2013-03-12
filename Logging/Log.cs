23ceaf57c366e276659973608b3bb8742f2445f1
iammadsnielsen <contact@madsnielsen.net>
2013-03-12 15:04:32 +0100

    Somewhere in the world, a honey badger died for excessive use of end tags...

--- Logging/Log.cs
+++ Logging/Log.cs
@@ -21,35 +21,34 @@ namespace Logging
         /// <param name="logType"></param>
         public static void LogMessage(Exception ex, string pageName, int logType)
         {
-                string getRemoteIp = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
-                if (String.IsNullOrEmpty(getRemoteIp))
-                    getRemoteIp = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            string getRemoteIp = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (String.IsNullOrEmpty(getRemoteIp))
+                getRemoteIp = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
 
-                StringBuilder strGetAllSessions = new StringBuilder();
-                foreach (string key in HttpContext.Current.Session.Keys)
+            StringBuilder strGetAllSessions = new StringBuilder();
+            foreach (string key in HttpContext.Current.Session.Keys)
+            {
+                if (key != "LastError" || key != "LastErrorPageName")
                 {
-                    if (key != "LastError" || key != "LastErrorPageName")
-                    {
-                        strGetAllSessions.Append("Session(");
-                        strGetAllSessions.Append(key);
-                        strGetAllSessions.Append(") - ");
-                        strGetAllSessions.Append(HttpContext.Current.Session[key]);
-                        strGetAllSessions.Append("<br/>");
-                    }
+                    strGetAllSessions.Append("Session(");
+                    strGetAllSessions.Append(key);
+                    strGetAllSessions.Append(") - ");
+                    strGetAllSessions.Append(HttpContext.Current.Session[key]);
+                    strGetAllSessions.Append("<br/>");
                 }
-
-                StringBuilder strGetAllHeaders = new StringBuilder();
-                NameValueCollection strAllHeaders = HttpContext.Current.Request.Headers;
-                for (int i = 0; i < strAllHeaders.Count; i++)
-                {
-                    strGetAllHeaders.Append(strAllHeaders.GetKey(i));
-                    strGetAllHeaders.Append(" = ");
-                    strGetAllHeaders.Append(strAllHeaders.Get(i));
-                    strGetAllHeaders.Append("<br/>");
-                }
-
-                //TODO: Add log message to json/xml file
             }
+
+            StringBuilder strGetAllHeaders = new StringBuilder();
+            NameValueCollection strAllHeaders = HttpContext.Current.Request.Headers;
+            for (int i = 0; i < strAllHeaders.Count; i++)
+            {
+                strGetAllHeaders.Append(strAllHeaders.GetKey(i));
+                strGetAllHeaders.Append(" = ");
+                strGetAllHeaders.Append(strAllHeaders.Get(i));
+                strGetAllHeaders.Append("<br/>");
+            }
+
+            //TODO: Add log message to json/xml file
         }
     }
 }
