b4b367d2f96b3338dbd1ddc20be53d095d52969f
Chris <christind@hotmail.com>
2013-03-13 11:50:22 +0100

    Repository classes added, there once was a small girl who lived with her grandmom

---
+++ Repository/Resources/BookmarkRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    class BookmarkRepository
+    {
+        private BPDbContext db;
+
+        public BookmarkRepository()
+        {
+            db = new BPDbContext();
+        }
+
+        public IQueryable<Bookmark> GetAllBookmarks()
+        {
+            return db.Bookmarks;
+        }
+
+        public Bookmark GetBookmarkById(int id)
+        {
+            return db.Bookmarks.FirstOrDefault(x => x.ID == id);
+        }
+
+        public void Insert(Bookmark bookmark)
+        {
+            db.Bookmarks.Add(bookmark);
+            db.SaveChanges();
+        }
+
+        public void Update(Bookmark bookmark)
+        {
+            Bookmark rBookmark = GetBookmarkById(bookmark.ID);
+            if (rBookmark == null)
+                return;
+
+            rBookmark.Description = bookmark.Description;
+            rBookmark.IsActive = bookmark.IsActive;
+            rBookmark.Title = bookmark.Title;
+            db.SaveChanges();
+        }
+
+        public void Delete(Bookmark bookmark)
+        {
+            if (GetBookmarkById(bookmark.ID) == null)
+                return;
+            db.Bookmarks.Remove(bookmark);
+            db.SaveChanges();
+        }
+    }
+}
