7a2c9e5601deb74d20c447b3054e93309346900e
Chris <christind@hotmail.com>
2013-03-13 08:42:01 +0100

    Added files

---
+++ Repository/Resources/ArchiveRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    class ArchiveRepository
+    {
+        private BPDbContext db;
+
+        public ArchiveRepository()
+        {
+            db = new BPDbContext();
+        }
+
+        public IQueryable<Archive> GetAllArchives()
+        {
+            return db.Archives;
+        }
+
+        public Archive GetArchiveById(int id)
+        {
+            return db.Archives.FirstOrDefault(x => x.ID == id);
+        }
+
+        public void Insert(Archive archive)
+        {
+            db.Archives.Add(archive);
+            db.SaveChanges();
+        }
+
+        public void Delete(Archive archive)
+        {
+            if (GetArchiveById(archive.ID) == null)
+                return;
+            db.Archives.Remove(archive);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Repository/Resources/ArchiveStationRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    class ArchiveStationRepository
+    {
+        private BPDbContext db;
+
+        public ArchiveStationRepository()
+        {
+            db = new BPDbContext();
+        }
+
+        public IQueryable<ArchiveStation> GetAllArchiveStations()
+        {
+            return db.ArchiveStations;
+        }
+
+        public ArchiveStation GetArchiveStationById(int id)
+        {
+            return db.ArchiveStations.FirstOrDefault(x => x.ID == id);
+        }
+
+        public void Insert(ArchiveStation archiveStation)
+        {
+            db.ArchiveStations.Add(archiveStation);
+            db.SaveChanges();
+        }
+
+        public void Update(ArchiveStation archiveStation)
+        {
+            ArchiveStation rArchiveStation = GetArchiveStationById(archiveStation.ID);
+            if (rArchiveStation == null)
+                return;
+
+            rArchiveStation.Archive = archiveStation.Archive;
+            db.SaveChanges();
+        }
+
+        public void Delete(ArchiveStation archiveStation)
+        {
+            if (GetArchiveStationById(archiveStation.ID) == null)
+                return;
+
+            db.ArchiveStations.Remove(archiveStation);
+            db.SaveChanges();
+        }
+    }
+}
