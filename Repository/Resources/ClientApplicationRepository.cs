015daf9b4caabd8c92871f24acc8b4fb90047a1f
iammadsnielsen <contact@madsnielsen.net>
2013-03-15 16:13:42 +0100

    More repos...

---
+++ Repository/Resources/ClientApplicationRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    class ClientApplicationRepository
+    {
+        private BPDbContext db;
+
+        public ClientApplicationRepository()
+        {
+            db = new BPDbContext();
+        }
+
+        public IQueryable<ClientApplication> GetAllClientApps()
+        {
+            return db.ClientApplications;
+        }
+
+        public ClientApplication GetClientAppById(int value)
+        {
+            return db.ClientApplications.FirstOrDefault(x => x.ID == value);
+        }
+
+        public void Insert(ClientApplication clientApplication)
+        {
+            db.ClientApplications.Add(clientApplication);
+            db.SaveChanges();
+        }
+
+        public void Update(ClientApplication clientApplication)
+        {
+            ClientApplication rClientApplication = GetClientAppById(clientApplication.ID);
+
+            if (rClientApplication == null)
+                return;
+
+            rClientApplication.Title = clientApplication.Title;
+            rClientApplication.Description = clientApplication.Description;
+            rClientApplication.AppKey = clientApplication.AppKey;
+            rClientApplication.CryptoKey = clientApplication.CryptoKey;
+            db.SaveChanges();
+        }
+
+        public void Delete(ClientApplication clientApplication)
+        {
+            if (GetClientAppById(clientApplication.ID) == null)
+                return;
+
+            db.ClientApplications.Remove(clientApplication);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Repository/Resources/ManufacturerRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    class ManufacturerRepository
+    {
+        private BPDbContext db;
+
+        public ManufacturerRepository()
+        {
+            db = new BPDbContext();
+        }
+
+        public IQueryable<Manufacturer> GetAllManufacturers()
+        {
+            return db.Manufacturers;
+        }
+
+        public Manufacturer GetManufacturerById(int value)
+        {
+            return db.Manufacturers.FirstOrDefault(x => x.ID == value);
+        }
+
+        public void Insert(Manufacturer manufacturer)
+        {
+            db.Manufacturers.Add(manufacturer);
+            db.SaveChanges();
+        }
+
+        public void Update(Manufacturer manufacturer)
+        {
+            Manufacturer rManufacturer = GetManufacturerById(manufacturer.ID);
+
+            if (rManufacturer == null)
+                return;
+
+            rManufacturer.Name = manufacturer.Name;
+            rManufacturer.Website = manufacturer.Website;
+        }
+
+        public void Delete(Manufacturer manufacturer)
+        {
+            if (GetManufacturerById(manufacturer.ID) == null)
+                return;
+
+            db.Manufacturers.Remove(manufacturer);
+            db.SaveChanges();
+        }
+    }
+}
