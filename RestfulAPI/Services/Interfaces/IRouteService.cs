08f969104469d53912b84616b5b1d6ac95bd0186
iammadsnielsen <contact@madsnielsen.net>
2013-04-25 12:50:25 +0200

    Rename graph to route, merged archive+archivestation into route service...

--- RestfulAPI/Services/Interfaces/IRouteService.cs
+++ RestfulAPI/Services/Interfaces/IRouteService.cs
@@ -5,7 +5,7 @@ using Repository.Models;
 namespace RestfulAPI.Services.Interfaces
 {
     [ServiceContract]
-    public interface IGraphService
+    public interface IRouteService
     {
         #region edges
 
@@ -30,6 +30,32 @@ namespace RestfulAPI.Services.Interfaces
 
         [OperationContract]
         bool CalculateRoute(string sloc, string eloc);
+
+        #endregion
+
+        #region archived entries
+
+        [OperationContract]
+        List<Archive> GetAllArchivedEntries();
+
+        [OperationContract]
+        Archive GetArchivedEntryById(string value);
+
+        [OperationContract]
+        bool CreateArchivedEntry(Archive archive);
+
+        #endregion
+
+        #region archived stations
+
+        [OperationContract]
+        List<ArchiveStation> GetAllArchivedStations();
+
+        [OperationContract]
+        ArchiveStation GetArchivedStationById(string value);
+
+        [OperationContract]
+        bool CreateArchivedStation(ArchiveStation archiveStation);
 
         #endregion
 
---
+++ RestfulAPI/Services/RouteService.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Web;
+using Logging;
+using Repository.Models;
+using Repository.Resources;
+using RestfulAPI.Services.Interfaces;
+
+namespace RestfulAPI.Services
+{
+    public class RouteService : IRouteService
+    {
+        private readonly EdgeRepository _edgeRepository;
+        private readonly ArchiveRepository _archiveRepository;
+        private readonly ArchiveStationRepository _archiveStationRepository;
+
+        public RouteService()
+        {
+            _edgeRepository = new EdgeRepository();
+            _archiveRepository = new ArchiveRepository();
+            _archiveStationRepository = new ArchiveStationRepository();
+        }
+
+        #region edges
+
+        public List<Edge> GetAllEdges()
+        {
+            try
+            {
+                return _edgeRepository.GetAllEdges().ToList();
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "GetAllEdges", 1, WebOperationContext.Current);
+                return null;
+            }
+        }
+
+        public Edge GetEdgeById(string value)
+        {
+            try
+            {
+                int edgeId;
+                if (Int32.TryParse(value, out edgeId))
+                {
+                    return _edgeRepository.GetEdgeById(edgeId);
+                }
+                throw new FormatException("The supplied id, is not of the datatype integer.");
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "GetEdgeById", 1, WebOperationContext.Current);
+                return null;
+            }
+        }
+
+        public bool CreateEdge(Edge edge)
+        {
+            try
+            {
+                _edgeRepository.Insert(edge);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "CreateEdge", 1, WebOperationContext.Current);
+                return false;
+            }
+        }
+
+        public bool UpdateEdge(Edge edge)
+        {
+            try
+            {
+                _edgeRepository.Update(edge);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "UpdateEdge", 1, WebOperationContext.Current);
+                return false;
+            }
+        }
+
+        public bool DisableEdge(string value)
+        {
+            try
+            {
+                int edgeId;
+                    if (Int32.TryParse(value, out edgeId))
+                    {
+                        _edgeRepository.Disable(edgeId);
+                        return true;
+                    }
+                    throw new FormatException("The supplied id, is not of the datatype integer.");
+
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "DisableEdge", 1, WebOperationContext.Current);
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region route calculation
+
+        public bool CalculateRoute(string sloc, string eloc)
+        {
+            try
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "CalculateRoute", 1, WebOperationContext.Current);
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region archived entries
+
+        public List<Archive> GetAllArchivedEntries()
+        {
+            try
+            {
+                return _archiveRepository.GetAllArchives().ToList();
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "GetAllArchivedEntries", 1, WebOperationContext.Current);
+                return null;
+            }
+        }
+
+        public Archive GetArchivedEntryById(string value)
+        {
+            try
+            {
+                int archiveId;
+                if (Int32.TryParse(value, out archiveId))
+                {
+                    return _archiveRepository.GetArchiveById(archiveId);
+                }
+                throw new FormatException("The supplied id, is not of the datatype integer.");
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "GetArchivedEntryById", 1, WebOperationContext.Current);
+                return null;
+            }
+        }
+
+        public bool CreateArchivedEntry(Archive archive)
+        {
+            try
+            {
+                _archiveRepository.Insert(archive);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "CreateArchivedEntry", 1, WebOperationContext.Current);
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region archived stations
+
+        public List<ArchiveStation> GetAllArchivedStations()
+        {
+            try
+            {
+                return _archiveStationRepository.GetAllArchiveStations().ToList();
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "GetAllArchivedStations", 1, WebOperationContext.Current);
+                return null;
+            }
+        }
+
+        public ArchiveStation GetArchivedStationById(string value)
+        {
+            try
+            {
+                int stationId;
+                if (Int32.TryParse(value, out stationId))
+                {
+                    return _archiveStationRepository.GetArchiveStationById(stationId);
+                }
+                throw new FormatException("The supplied id, is not of the datatype integer.");
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "GetArchivedStationById", 1, WebOperationContext.Current);
+                return null;
+            }
+        }
+
+        public bool CreateArchivedStation(ArchiveStation archiveStation)
+        {
+            try
+            {
+                _archiveStationRepository.Insert(archiveStation);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                HandleLogging.LogMessage(ex, "CreateArchivedStation", 1, WebOperationContext.Current);
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
