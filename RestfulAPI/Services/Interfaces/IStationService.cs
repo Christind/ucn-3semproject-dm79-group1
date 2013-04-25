0023c1be62e13d387ed9090b016ca3593dfe45a2
Chris <christind@hotmail.com>
2013-04-25 11:05:47 +0200

    changed services to be contained without one class to provide better clarity :)

--- RestfulAPI/Services/Interfaces/IStationService.cs
+++ RestfulAPI/Services/Interfaces/IStationService.cs
@@ -7,6 +7,8 @@ namespace RestfulAPI.Services.Interfaces
     [ServiceContract]
     public interface IStationService
     {
+        #region Station
+
         [OperationContract]
         List<Station> GetAllStations();
 
@@ -15,5 +17,58 @@ namespace RestfulAPI.Services.Interfaces
 
         [OperationContract]
         bool ReserveBattery(string stationId, string userId);
+
+        #endregion
+
+        #region StationMaintenance
+
+        [OperationContract]
+        List<StationMaintenance> GetAllStationMaintenances();
+
+        [OperationContract]
+        StationMaintenance GetStationMaintenanceById(string id);
+
+        [OperationContract]
+        bool EditStationMaintenance(StationMaintenance stationMaintenance);
+
+        [OperationContract]
+        bool InsertStationMaintenance(StationMaintenance stationStationMaintenance);
+
+        #endregion
+
+        #region MaintenanceType
+
+        [OperationContract]
+        List<MaintenanceType> GetAllMaintenanceTypes();
+
+        [OperationContract]
+        MaintenanceType GetMaintenanceTypeById(string id);
+
+        [OperationContract]
+        bool EditMaintenanceType(MaintenanceType maintenanceType);
+
+        [OperationContract]
+        bool InsertMaintenanceType(MaintenanceType maintenanceType);
+
+        #endregion
+
+        #region Battery
+
+        [OperationContract]
+        List<Battery> GetAllBatteries();
+
+        [OperationContract]
+        Battery GetBatteryById(string id);
+
+        [OperationContract]
+        List<Battery> GetBatteriesByStatus(string status);
+
+        [OperationContract]
+        bool EditBattery(Battery battery);
+
+        [OperationContract]
+        bool InsertBattery(Battery battery);
+
+        #endregion
     }
 }
--- RestfulAPI/Services/StationService.cs
+++ RestfulAPI/Services/StationService.cs
@@ -9,12 +9,21 @@ namespace RestfulAPI.Services
 {
     public class StationService : IStationService
     {
-        private StationRepository _stationRepository;
+        private readonly StationRepository _stationRepository;
+        private readonly StationMaintenanceRepository _stationMaintenanceRepository;
+        private readonly MaintenanceTypeRepository _maintenanceTypeRepository;
+        private readonly BatteryRepository _batteryRepository;
+
 
         public StationService()
         {
             _stationRepository = new StationRepository();
+            _stationMaintenanceRepository = new StationMaintenanceRepository();
+            _maintenanceTypeRepository = new MaintenanceTypeRepository();
+            _batteryRepository = new BatteryRepository();
         }
+
+        #region Station
 
         public List<Station> GetAllStations()
         {
@@ -48,13 +57,13 @@ namespace RestfulAPI.Services
                     battery.Status = 3;
 
                     var reservation = new Reservation
-                        {
-                            UserId = user.ID,
-                            StationId = station.ID,
-                            CreatedDate = DateTime.Now,
-                            ExpiredDate = DateTime.Now.AddDays(1),
-                            IsActive = true
-                        };
+                    {
+                        UserId = user.ID,
+                        StationId = station.ID,
+                        CreatedDate = DateTime.Now,
+                        ExpiredDate = DateTime.Now.AddDays(1),
+                        IsActive = true
+                    };
 
                     reservationRepo.Insert(reservation);
                     batteryRepo.Update(battery);
@@ -63,5 +72,151 @@ namespace RestfulAPI.Services
 
             return false;
         }
+
+        #endregion
+
+        #region StationMaintenance
+
+        public List<StationMaintenance> GetAllStationMaintenances()
+        {
+            return _stationMaintenanceRepository.GetAllStationMaintenances().ToList();
+        }
+
+        public StationMaintenance GetStationMaintenanceById(string id)
+        {
+            return _stationMaintenanceRepository.GetStationMaintenanceById(Convert.ToInt32(id));
+        }
+
+        public bool EditStationMaintenance(StationMaintenance stationMaintenance)
+        {
+            try
+            {
+                if (stationMaintenance == null)
+                    return false;
+
+                _stationMaintenanceRepository.Update(stationMaintenance);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool InsertStationMaintenance(StationMaintenance stationStationMaintenance)
+        {
+            try
+            {
+                if (stationStationMaintenance == null)
+                    return false;
+
+                _stationMaintenanceRepository.Insert(stationStationMaintenance);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region MaintenanceType
+
+        public List<MaintenanceType> GetAllMaintenanceTypes()
+        {
+            return _maintenanceTypeRepository.GetAllMaintenanceTypes().ToList();
+        }
+
+        public MaintenanceType GetMaintenanceTypeById(string id)
+        {
+            return _maintenanceTypeRepository.GetMaintenanceTypeById(Convert.ToInt32(id));
+        }
+
+        public bool EditMaintenanceType(MaintenanceType maintenanceType)
+        {
+            try
+            {
+                if (maintenanceType == null)
+                    return false;
+
+                _maintenanceTypeRepository.Update(maintenanceType);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool InsertMaintenanceType(MaintenanceType maintenanceType)
+        {
+            try
+            {
+                if (maintenanceType == null)
+                    return false;
+
+                _maintenanceTypeRepository.Insert(maintenanceType);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Battery
+
+        public List<Battery> GetAllBatteries()
+        {
+            return _batteryRepository.GetAllBatteries().ToList();
+        }
+
+        public Battery GetBatteryById(string id)
+        {
+            return _batteryRepository.GetBatteryById(Convert.ToInt32(id));
+        }
+
+        public List<Battery> GetBatteriesByStatus(string status)
+        {
+            var batteryList = _batteryRepository.GetAllBatteries().ToList();
+            return batteryList.Where(battery => battery.Status == (Convert.ToInt32(status))).ToList();
+        }
+
+        public bool EditBattery(Battery battery)
+        {
+            try
+            {
+                if (battery == null)
+                    return false;
+
+                _batteryRepository.Update(battery);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool InsertBattery(Battery battery)
+        {
+            try
+            {
+                if (battery == null)
+                    return false;
+
+                _batteryRepository.Insert(battery);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        #endregion
     }
 }
