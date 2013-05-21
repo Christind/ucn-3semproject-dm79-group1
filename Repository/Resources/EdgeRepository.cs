0556c9f5d1ea8906509bc3587991dadb12d6f0de
Chris <christind@hotmail.com>
2013-05-21 15:13:56 +0200

    added a way to find edges from startlocation

--- Repository/Resources/EdgeRepository.cs
+++ Repository/Resources/EdgeRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
 using System.Linq;
 using Repository.Models;
 
@@ -76,6 +78,34 @@ namespace Repository.Resources
         public Edge GetEdgeByStartEndStation(Station start, Station end)
         {
             return db.Edges.AsNoTracking().FirstOrDefault(x => x.StartStationId.Equals(start.ID) && x.EndStationId.Equals(end.ID));
+        }
+
+        public List<Edge> GetEdgesByLocation(string _lat, string _lng)
+        {
+            _lat = _lat.Replace(".", ",");
+            _lng = _lng.Replace(".", ",");
+            double lat;
+            decimal lat2 = Convert.ToDecimal(_lat);
+            int lng;
+            if (Double.TryParse(_lat.Substring(0, _lat.IndexOf(',') + 1), out lat) && Int32.TryParse(_lng, out lng))
+            {
+                GeoCoordinate geoCoordinate = new GeoCoordinate(lat, lng);
+                var stations = db.Stations.Where(x => (Int32)x.StationLong == lng && Math.Round(x.StationLat,1) == lat2);
+                List<Edge> edges = new List<Edge>();
+                foreach (var station in stations)
+                {
+                    double distance =
+                        geoCoordinate.GetDistanceTo(new GeoCoordinate(Convert.ToDouble(station.StationLat),
+                                                                      Convert.ToDouble(station.StationLong)));
+
+                    Edge edge = new Edge{Distance = Convert.ToDecimal(distance), EndStationId = station.ID, IsActive = true};
+                    edges.Add(edge);
+                }
+
+                return edges;
+            }
+
+            return null;
         }
     }
 }
